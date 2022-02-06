3a0617a8a103d5285a73e282913871bed6c46d17
badtheus <94135584+badtheus@users.noreply.github.com>
2022-02-06 19:06:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById("res")
 
-    if (hora >= 0 && hora < 12) {
-        // BOM DIA!
-        img.src='manha.jpg'
-        document.body.style.background = '#e2cd9f'
-    } else if (hora >= 12 && hora <= 18 ) {
-        // BOA TARDE!
-        img.src='tarde.jpg'
-        document.body.style.background = '#b9846f'
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel Contar! '
+        window.alert('[ERRO] Faltam dados! ')
+        
     } else {
-    // BOA NOITE!
-        img.src='noite.jpg'
-        document.body.style.background ='#515154'
- }
+      res.innerHTML = 'Contando:'
+      let i = Number(ini.value)
+      let f = Number(fim.value)
+      let p = Number(passo.value)
+      if (p <= 0) {
+          window.alert('Passo inválido! Considere passo 1')
+          p = 1 
+      }
+      if (i < f) {
+          // Contagem crescente
+          for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F603} `
+        }
+      } else {
+          // contagem regressiva
+          for (let c= i; c >= f; c -= p ) {
+              res.innerHTML += ` ${c} \u{1F603}`
+          }
+      }
+      res.innerHTML += `\u{1F3C1} `
+   }
 }
