27216cd0e741644f0e45b8a5d95694614a5cd05a
badtheus <94135584+badtheus@users.noreply.github.com>
2022-02-07 21:11:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
-function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById("res")
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossivel Contar! '
-        window.alert('[ERRO] Faltam dados! ')
-        
+function tabuada() {
+        let num = document.getElementById('txtn') ;
+        let tab = document.getElementById('seltab') ;
+    
+    if(num.value.length == 0 ) {
+        window.alert('Por favor,digite um número!') 
     } else {
-      res.innerHTML = 'Contando:'
-      let i = Number(ini.value)
-      let f = Number(fim.value)
-      let p = Number(passo.value)
-      if (p <= 0) {
-          window.alert('Passo inválido! Considere passo 1')
-          p = 1 
-      }
-      if (i < f) {
-          // Contagem crescente
-          for(let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{1F603} `
+        let n = Number(num.value)
+        let c = 1 
+        tab.innerHTML = ' '
+        while (c <= 10 ) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c} `
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
-      } else {
-          // contagem regressiva
-          for (let c= i; c >= f; c -= p ) {
-              res.innerHTML += ` ${c} \u{1F603}`
-          }
-      }
-      res.innerHTML += `\u{1F3C1} `
-   }
+    }
 }
